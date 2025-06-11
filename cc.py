b92c40a17dced7f2b316a7ec2668e9f1d0eafcd2
ZeToF! <zetof@zetof.net>
2025-06-11 23:09:02 +0200

    First value widget not perfect

--- cc.py
+++ cc.py
@@ -1,13 +1,24 @@
 import curses
+from enum import Enum
 from threading import Thread
 from time import sleep
 from w_value import W_Value
 
 
+class TC(Enum):
+    LABEL = 1
+    BUTTON = 2
+    VAL = 3
+
+
 class CC(Thread):
 
-    def __init__(self):
+    def __init__(self, label_color=209, button_color=221, value_color=155):
+        self._label_color = label_color
+        self._button_color = button_color
+        self._value_color = value_color
         self._widgets = []
+        self._active_widget = None
         self._running = True
         self._delay = .1
 
@@ -17,6 +28,9 @@ class CC(Thread):
         window.nodelay(True)
         self.max_col = window_size[0] - 1
         self.max_line = window_size[1] - 1
+        curses.init_pair(1, self._label_color, 0)
+        curses.init_pair(2, self._button_color, 0)
+        curses.init_pair(3, self._value_color, 0)
         self._window = window
         self.draw_all()
 
@@ -24,11 +38,13 @@ class CC(Thread):
         self._init_params(window)
         while self._running:
             try:
-                key = window.getkey()
-                if str(key) == 'q':
+                key = str(window.getkey())
+                if key == 'q':
                     self.stop()
-                if self.select_widget(key):
-                    self.stop()
+                elif key in ['-', '+']:
+                    self.send_key_to_widget(key)
+                else:
+                    self.select_widget(key)
             except Exception:
                 pass
             sleep(self._delay)
@@ -40,9 +56,6 @@ class CC(Thread):
     def stop(self):
         self._running = False
 
-    def set_theme(self, val_color, button_color, label_color):
-        pass
-
     def add_w_value(self, low, high, val, label, col, line):
         self._widgets.append(W_Value(low, high, val, label, col, line))
 
@@ -53,5 +66,8 @@ class CC(Thread):
     def select_widget(self, key):
         for widget in self._widgets:
             if widget.get_key() == key:
-                return True
-        return False
+                self._active_widget = widget
+
+    def send_key_to_widget(self, key):
+        if self._active_widget.send_key(key):
+            self._active_widget.draw(self._window)
--- w_value.py
+++ w_value.py
@@ -8,13 +8,25 @@ class W_Value(Widget):
         self._low = low
         self._high = high
         self._val = val
+        self._selected = False
         super().__init__(label, line, col)
 
     def draw(self, window):
-        window.addstr(self._line, self._col, self._label[0])
-        window.addstr(self._key, curses.A_STANDOUT)
-        window.addstr("{}: ".format(self._label[1]))
-        window.addstr('-')
-        window.addstr(" [ {} ] ".format(self._val))
-        window.addstr('+')
+        window.addstr(self._line, self._col,
+                      self._label[0], curses.color_pair(1))
+        window.addstr(self._key, curses.color_pair(1) | curses.A_STANDOUT)
+        window.addstr("{}: ".format(self._label[1]), curses.color_pair(1))
+        window.addstr('-', curses.color_pair(2))
+        window.addstr(" [ {} ] ".format(self._val),
+                      curses.color_pair(3) | curses.A_BOLD)
+        window.addstr('+', curses.color_pair(2))
         window.refresh()
+
+    def send_key(self, key):
+        if key == '+' and self._val < self._high:
+            self._val += 1
+            return True
+        if key == '-' and self._val > self._low:
+            self._val -= 1
+            return True
+        return False
